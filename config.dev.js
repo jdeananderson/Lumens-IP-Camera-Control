d29a773a5b7a485a1a1940c7f11d5d3e60373fb8
Craig Moon <cmoon@rocketlawyer.com>
2022-11-01 20:09:57 -0600

    added separate config for development

---
+++ config.dev.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        id: "1",
+        name: "Home",
+        hostname: "192.168.29.198",
+        port: 8080,
+        username: "admin",
+        password: "9999",
+        presetLocks: [
+            // "preset000",
+            // "preset001",
+            // "preset002",
+            // "preset003",
+            // "preset004",
+        ],
+        whiteBalance: {mode: "MANUAL", red: 20, blue: 128},
+        snapshotUrl: "",
+    }
+];
